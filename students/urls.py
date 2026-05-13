f9b1eadf350879b7cd5318a0729406434ad9c828
Wargave <animediamond08@gmail.com>
2026-05-13 14:34:06 +0500

    Update urls.py

--- students/urls.py
+++ students/urls.py
@@ -2,11 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('students/', views.student_list, name='student_list'),
-    path('students/add/', views.add_student, name='add_student'),
-    path('students/<int:pk>/', views.student_detail, name='student_detail'),
-    path('students/<int:pk>/edit/', views.edit_student, name='edit_student'),
-    path('students/<int:pk>/delete/', views.delete_student, name='delete_student'),
+    # Теперь этот путь будет открываться по адресу /students/
+    path('', views.student_list, name='student_list'),
+    
+    # Этот по адресу /students/add/
+    path('add/', views.add_student, name='add_student'),
+    
+    # Этот по адресу /students/1/
+    path('<int:pk>/', views.student_detail, name='student_detail'),
+    
+    # Этот по адресу /students/1/edit/
+    path('<int:pk>/edit/', views.edit_student, name='edit_student'),
+    
+    # Этот по адресу /students/1/delete/
+    path('<int:pk>/delete/', views.delete_student, name='delete_student'),
+    
     path('logout/', views.logout_view, name='logout'),
 ]
 
