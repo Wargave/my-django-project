fe03de6d5616449515108837f2500256ca21a7b0
Wargave <animediamond08@gmail.com>
2026-05-13 14:05:22 +0500

    Update urls.p

--- college_site/urls.py
+++ college_site/urls.py
@@ -7,7 +7,7 @@ from students import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.student_list, name='student_list'),
+    path('students/', views.student_list, name='student_list'),
     path("register/", views.register, name="register"),
     path("login/", auth_views.LoginView.as_view(template_name="students/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
